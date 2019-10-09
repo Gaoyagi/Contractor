f01ec50a0139888b4194aaf0b8aa1879cecd6df5
George Aoyagi <aoyagigeorge@gmail.com>
2019-10-08 20:16:01 -0700

    got a working page ish

--- app.py
+++ app.py
@@ -7,17 +7,16 @@ controllers = db.controllers
 
 app = Flask(__name__)
 
-# # OUR MOCK ARRAY OF PROJECTS
-# consoles = [
-#     { 'console': 'Xbox One', 'description': 'XBONE controllers' },
-#     { 'console': 'Playstation 4', 'description': 'PS4 Dual Shocks' },
-#     { 'console': 'Nintendo Switch', 'description': 'Joy Cons and Pro controllers' }
-# ]
+consoles = [
+    { 'console': 'Xbox One', 'description': 'XBONE controllers' },
+    { 'console': 'Playstation 4', 'description': 'PS4 Dual Shocks' },
+    { 'console': 'Nintendo Switch', 'description': 'Joy Cons and Pro controllers' }
+]
 
 #Home page view 
 @app.route('/')
 def index():
-    return render_template('contract_index.html', controllers = controllers.find())
+    return render_template('contract_index.html', consoles = consoles)
 
 # #ps5 controller page
 # @app.route('/consoles/PS4/')
@@ -32,12 +31,12 @@ def controller_new():
 #posts the new console to  the database
 @app.route('/controller', methods=['POST'])
 def controller_submit():
-     controller = {
+    controller = {
         'console': request.form.get('console'),
         'controller': request.form.get('controller')
     }
     controllers.insert_one(controller)
-    return redirect(url_for('contract_index'))
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
