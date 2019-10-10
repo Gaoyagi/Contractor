483882e012a9abc25cce6a5bb1e9141643a8b115
George Aoyagi <aoyagigeorge@gmail.com>
2019-10-10 00:19:51 -0700

    connecting mongo

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 
-client = MongoClient()
-db = client.Contractor
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/cards')
+client = MongoClient(host=f'{host}?retryWrites=false')
+db = client.get_default_database()
 
 #collections
 decks = db.decks   #currently whats  in stock
@@ -51,4 +52,4 @@ def cartDeleteAll():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
